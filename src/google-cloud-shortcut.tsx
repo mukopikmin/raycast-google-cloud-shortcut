4deecdb8483f62c09ba26e8348743157e76a703f
Yusuke Mukai <mukopikmin@gmail.com>
2026-02-09 00:29:01 +0900

    Refactor project management: move project-related functions to a new file

--- src/google-cloud-shortcut.tsx
+++ src/google-cloud-shortcut.tsx
@@ -1,51 +1,7 @@
 import { ActionPanel, Action, Icon, List } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { ProjectsClient } from "@google-cloud/resource-manager";
-import { LocalStorage } from "@raycast/api";
 import { ServiceList } from "./service-list";
-
-async function listProjects() {
-  const client = new ProjectsClient();
-
-  const projects: Project[] = [];
-  const iterable = client.searchProjectsAsync({});
-
-  for await (const project of iterable) {
-    projects.push({
-      id: project.projectId ?? "",
-      name: project.displayName ?? "",
-    });
-  }
-
-  return projects;
-}
-
-const CACHE_KEY_PROJECTS = "projects";
-
-async function listCachedProjects() {
-  const cachedProjects = await LocalStorage.getItem<string>(CACHE_KEY_PROJECTS);
-
-  if (cachedProjects === undefined) {
-    return undefined;
-  }
-
-  return JSON.parse(cachedProjects) as Project[];
-}
-
-async function cacheProjects(projects: Project[]) {
-  await LocalStorage.setItem(CACHE_KEY_PROJECTS, JSON.stringify(projects));
-}
-
-async function updateProjects(callback: (projects: Project[]) => void) {
-  const fetchedProjects = await listProjects();
-  cacheProjects(fetchedProjects);
-  callback(fetchedProjects);
-}
-
-type Project = {
-  id: string;
-  name: string;
-};
+import { listCachedProjects, Project, updateProjects } from "./project";
 
 export default function Command() {
   const [projects, setProjects] = useState<Project[] | undefined>();
---
+++ src/project.ts
@@ -0,0 +1,45 @@
+import { ProjectsClient } from "@google-cloud/resource-manager";
+import { LocalStorage } from "@raycast/api";
+
+const CACHE_KEY_PROJECTS = "projects";
+
+export type Project = {
+  id: string;
+  name: string;
+};
+
+export async function listProjects() {
+  const client = new ProjectsClient();
+
+  const projects: Project[] = [];
+  const iterable = client.searchProjectsAsync({});
+
+  for await (const project of iterable) {
+    projects.push({
+      id: project.projectId ?? "",
+      name: project.displayName ?? "",
+    });
+  }
+
+  return projects;
+}
+
+export async function listCachedProjects() {
+  const cachedProjects = await LocalStorage.getItem<string>(CACHE_KEY_PROJECTS);
+
+  if (cachedProjects === undefined) {
+    return undefined;
+  }
+
+  return JSON.parse(cachedProjects) as Project[];
+}
+
+export async function cacheProjects(projects: Project[]) {
+  await LocalStorage.setItem(CACHE_KEY_PROJECTS, JSON.stringify(projects));
+}
+
+export async function updateProjects(callback: (projects: Project[]) => void) {
+  const fetchedProjects = await listProjects();
+  cacheProjects(fetchedProjects);
+  callback(fetchedProjects);
+}
