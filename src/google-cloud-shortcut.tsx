8307765c2a0f7720c32a47148ee8827ea57424af
Yusuke Mukai <mukopikmin@gmail.com>
2026-02-09 21:15:05 +0900

    Refactor: move project list rendering to a separate component for better organization

--- src/google-cloud-shortcut.tsx
+++ src/google-cloud-shortcut.tsx
@@ -1,36 +1,7 @@
-import { ActionPanel, Action, Icon, List } from "@raycast/api";
-import { ServiceList } from "./service-list";
-import { useProjects } from "./use-projects";
+import { ProjectList } from "./project-list";
 
 export const Command = () => {
-  const { projects, isLoading, updateProjects } = useProjects();
-  console.log({ isLoading });
-  return (
-    <List isLoading={isLoading}>
-      {projects?.map((project) => (
-        <List.Item
-          key={project.id}
-          icon={Icon.Cloud}
-          title={project.name}
-          subtitle={project.id}
-          actions={
-            <ActionPanel>
-              <Action.Push title="Show Google Cloud Services" target={<ServiceList projectId={project.id} />} />
-            </ActionPanel>
-          }
-        />
-      ))}
-      <List.Item
-        icon={Icon.CircleProgress}
-        title={isLoading ? "Updating Projects ..." : "Update Projects"}
-        actions={
-          <ActionPanel>
-            <Action title="Update Projects" onAction={updateProjects} />
-          </ActionPanel>
-        }
-      />
-    </List>
-  );
+  return <ProjectList />;
 };
 
 export default Command;
---
+++ src/project-list.tsx
@@ -0,0 +1,34 @@
+import { ActionPanel, Action, Icon, List } from "@raycast/api";
+import { ServiceList } from "./service-list";
+import { useProjects } from "./use-projects";
+
+export const ProjectList = () => {
+  const { projects, isLoading, updateProjects } = useProjects();
+
+  return (
+    <List isLoading={isLoading}>
+      {projects?.map((project) => (
+        <List.Item
+          key={project.id}
+          icon={Icon.Cloud}
+          title={project.name}
+          subtitle={project.id}
+          actions={
+            <ActionPanel>
+              <Action.Push title="Show Google Cloud Services" target={<ServiceList projectId={project.id} />} />
+            </ActionPanel>
+          }
+        />
+      ))}
+      <List.Item
+        icon={Icon.CircleProgress}
+        title={isLoading ? "Updating Projects ..." : "Update Projects"}
+        actions={
+          <ActionPanel>
+            <Action title="Update Projects" onAction={updateProjects} />
+          </ActionPanel>
+        }
+      />
+    </List>
+  );
+};
