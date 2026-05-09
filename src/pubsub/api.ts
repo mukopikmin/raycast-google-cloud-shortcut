acdf264bb7a8d3558bbfe28198703b31199cd3a1
Yusuke Mukai <dev@muko.app>
2026-05-09 16:04:41 +0900

    feat: support progressive pagination for Pub/Sub resources

--- src/pubsub/api.ts
+++ src/pubsub/api.ts
@@ -9,6 +9,7 @@ type PubSubSubscriptionsResponse = {
       pushEndpoint: string;
     };
   }[];
+  nextPageToken?: string;
 };
 
 /**
@@ -18,47 +19,78 @@ export const listPubSubSubscriptions = async (
   projectId: string,
   accessToken: string,
 ): Promise<PubSubSubscription[]> => {
-  const body = await fetchGoogleApi<PubSubSubscriptionsResponse>(
-    `https://pubsub.googleapis.com/v1/projects/${projectId}/subscriptions`,
-    accessToken,
-  );
+  const subscriptions: PubSubSubscription[] = [];
+  let pageToken: string | undefined;
 
-  return (
-    body.subscriptions?.map((subscription) => {
-      return createPubSubSubscription({
-        projectId,
-        // projects/PROJECT_ID/subscriptions/SUBSCRIPTION_ID
-        name: subscription.name.split("/")[3] ?? "",
-        // projects/PROJECT_ID/topics/TOPIC_ID
-        topic: subscription.topic.split("/")[3] ?? "",
-        subscriptionType: subscription.pushConfig === undefined ? "Pull" : "Push",
-      });
-    }) ?? []
-  );
+  do {
+    const query = new URLSearchParams();
+    if (pageToken) {
+      query.set("pageToken", pageToken);
+    }
+    const suffix = query.toString();
+
+    const body = await fetchGoogleApi<PubSubSubscriptionsResponse>(
+      `https://pubsub.googleapis.com/v1/projects/${projectId}/subscriptions${suffix ? `?${suffix}` : ""}`,
+      accessToken,
+    );
+
+    subscriptions.push(
+      ...(body.subscriptions?.map((subscription) => {
+        return createPubSubSubscription({
+          projectId,
+          // projects/PROJECT_ID/subscriptions/SUBSCRIPTION_ID
+          name: subscription.name.split("/")[3] ?? "",
+          // projects/PROJECT_ID/topics/TOPIC_ID
+          topic: subscription.topic.split("/")[3] ?? "",
+          subscriptionType: subscription.pushConfig === undefined ? "Pull" : "Push",
+        });
+      }) ?? []),
+    );
+
+    pageToken = body.nextPageToken;
+  } while (pageToken);
+
+  return subscriptions;
 };
 
 type PubSubTopicsResponse = {
   topics?: {
     name: string;
   }[];
+  nextPageToken?: string;
 };
 
 /**
  * @see https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics/list
  */
 export const listPubSubTopics = async (projectId: string, accessToken: string): Promise<PubSubTopic[]> => {
-  const body = await fetchGoogleApi<PubSubTopicsResponse>(
-    `https://pubsub.googleapis.com/v1/projects/${projectId}/topics`,
-    accessToken,
-  );
+  const topics: PubSubTopic[] = [];
+  let pageToken: string | undefined;
 
-  return (
-    body.topics?.map((topic) => {
-      return createPubSubTopic({
-        projectId,
-        // projects/PROJECT_ID/topics/TOPIC_ID
-        name: topic.name.split("/")[3] ?? "",
-      });
-    }) ?? []
-  );
+  do {
+    const query = new URLSearchParams();
+    if (pageToken) {
+      query.set("pageToken", pageToken);
+    }
+    const suffix = query.toString();
+
+    const body = await fetchGoogleApi<PubSubTopicsResponse>(
+      `https://pubsub.googleapis.com/v1/projects/${projectId}/topics${suffix ? `?${suffix}` : ""}`,
+      accessToken,
+    );
+
+    topics.push(
+      ...(body.topics?.map((topic) => {
+        return createPubSubTopic({
+          projectId,
+          // projects/PROJECT_ID/topics/TOPIC_ID
+          name: topic.name.split("/")[3] ?? "",
+        });
+      }) ?? []),
+    );
+
+    pageToken = body.nextPageToken;
+  } while (pageToken);
+
+  return topics;
 };
